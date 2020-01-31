1337a3c4c91e7b09d039fa68bbd354e48b50fc3f
Keshav Kumar <keshavmca12@gmail.com>
2020-01-31 14:29:40 +0530

    java 8 practice

---
+++ src/com/design/pattern/Singleton.java
@@ -0,0 +1,12 @@
+package com.design.pattern;
+
+/**
+ * @Autor : Keshav Kumar 
+ * @Date : 07-Jan-2020
+ */
+
+public class Singleton {
+
+}
+
+
---
+++ src/com/thread/OddEvenThread.java
@@ -0,0 +1,59 @@
+package com.thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Autor : Keshav Kumar
+ * @Date : 31-Dec-2019
+ */
+
+public class OddEvenThread {
+
+	public static void main(String[] args) {
+		/*
+		 * Runnable print = () -> { while (atomicNumber.get() < 10) {
+		 * synchronized (atomicNumber) { if ((atomicNumber.get() % 2 == 0) &&
+		 * "Even".equals(Thread.currentThread().getName())) {
+		 * System.out.println("Even" + ":" + atomicNumber.getAndIncrement()); }
+		 * else if ((atomicNumber.get() % 2 != 0) &&
+		 * "Odd".equals(Thread.currentThread().getName())) {
+		 * System.out.println("Odd" + ":" + atomicNumber.getAndIncrement()); } }
+		 * } };
+		 */
+
+		Thread t1 = new Thread(new Print());
+		// t1.setName("Even");
+		t1.start();
+		Thread t2 = new Thread(new Print());
+		// t2.setName("Odd");
+		t2.start();
+
+	}
+
+}
+
+class Print implements Runnable {
+	static AtomicInteger atomicNumber = new AtomicInteger(1);
+
+	@Override
+	public void run() {
+		while (atomicNumber.get() < 10) {
+			synchronized (atomicNumber) {
+				if ((atomicNumber.get()
+						% 2 == 0) /*
+									 * && "Even".equals(Thread.currentThread().
+									 * getName())
+									 */) {
+					System.out.println("Even" + ":" + atomicNumber.getAndIncrement());
+				} else if ((atomicNumber.get()
+						% 2 != 0) /*
+									 * && "Odd".equals(Thread.currentThread().
+									 * getName())
+									 */) {
+					System.out.println("Odd" + ":" + atomicNumber.getAndIncrement());
+				}
+			}
+		}
+	}
+
+}
